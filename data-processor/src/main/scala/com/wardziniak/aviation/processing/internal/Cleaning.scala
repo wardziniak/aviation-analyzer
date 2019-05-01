1a191b13e868fe5165d402484e2317784b913e12
bwardzinski <bwardzinski@pl.imshealth.com>
2019-05-01 18:32:00 +0200

    Cleaning Processor: 1. Checking if destination/departure airport has changed 2. If is at least some number of data for group

--- data-processor/src/main/scala/com/wardziniak/aviation/processing/internal/Cleaning.scala
+++ data-processor/src/main/scala/com/wardziniak/aviation/processing/internal/Cleaning.scala
@@ -8,17 +8,33 @@ import org.apache.kafka.streams.processor.AbstractProcessor
   * Punctuator with cleaning should take few snapshot for same flight and try to figure out if all are ok
   * Drop data if:
   * 1. Destination airport has changed
-  * 2. Not enough data is grouped < 5~10
+  * 2. Departure airport has changed
+  * 3. Not enough data is grouped < 5~10
   */
 
 object Cleaning {
+
+  val MinimalGroupSize = 8
+
+  val SameDestinationAirportPredicate: InAirFlightData => Boolean =
+    inAirFlightData => inAirFlightData.flightInfo.map(_.arrival.iata).distinct.size == 1
+
+  val SameDepartureAirportPredicate: InAirFlightData => Boolean =
+    inAirFlightData => inAirFlightData.flightInfo.map(_.departure.iata).distinct.size == 1
+
+  val GroupSizePredicate: InAirFlightData => Boolean =
+    inAirFlightData => inAirFlightData.flightInfo.size > MinimalGroupSize
+
+  lazy val CleaningPredicates: Seq[InAirFlightData => Boolean] = Seq(
+    SameDestinationAirportPredicate, SameDepartureAirportPredicate, GroupSizePredicate
+  )
+
   case class CleaningProcessor() extends AbstractProcessor[FlightNumberIata, InAirFlightData] {
     override def process(flightNumberIata: FlightNumberIata, inAirFlightData: InAirFlightData): Unit = {
-      // TODO check flight pattern, if following snapshot are in proper route
-      val uniqueDeparture = inAirFlightData.flightInfo.map(_.departure.iata).distinct.size
-      val arrivalDeparture = inAirFlightData.flightInfo.map(_.arrival.iata).distinct.size
-      if (uniqueDeparture == 1 && arrivalDeparture == 1)
-        context().forward(flightNumberIata, inAirFlightData)
+      CleaningPredicates
+        .foldLeft(Some(inAirFlightData): Option[InAirFlightData])((optInAirFlightData, predicate) => optInAirFlightData.filter(predicate))
+        .foreach(inAirFlightData => context().forward(flightNumberIata, inAirFlightData))
     }
   }
+
 }
